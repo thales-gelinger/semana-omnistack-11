93c3cdd15bac1cac4ef540d714e900d76b732b57
thales-gelinger <thalesgelinger99@gmail.com>
2020-03-24 12:05:57 -0300

    feat: add main funcionalities

---
+++ backend/src/database/migrations/20200324090504_create_ongs.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex) {
+  const { schema } = knex
+
+  return schema.createTable('ongs', (table) => {
+      table.string('id').primary()
+      table.string('name').notNullable()
+      table.string('email').notNullable()
+      table.string('whatsapp').notNullable()
+      table.string('city').notNullable()
+      table.string('uf', 2).notNullable()
+  })
+
+}
+
+exports.down = function(knex) {
+    const { schema } = knex
+
+    return schema.dropTable('ongs')
+}
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,23 @@
 import { Router } from "express"
+import OngController from "./controllers/OngController"
+import IncidentController from "./controllers/IncidentController"
+import ProfileController from "./controllers/ProfileController"
+import SessionController from "./controllers/SessionController"
 
 const routes = Router()
 
-routes.get("/", (req, res) => {
-    res.send("Hello backend")
-})
+routes.post('/sessions', SessionController.store)
+
+routes.get('/ongs', OngController.index)
+
+routes.post('/ongs', OngController.store)
+
+routes.get('/profiles', ProfileController.index)
+
+routes.get('/incidents', IncidentController.index)
+
+routes.post('/incidents', IncidentController.store)
+
+routes.delete('/incidents/:id', IncidentController.destroy)
 
 export default routes
